114ed375d5ea59916a370671220162f7e48e1624
emailman <emailman@dtcc.edu>
2016-09-04 11:57:07 -0400

    Demo for reading a file, building a list box, and the form load event.

--- CIS 209 Chapter 5 Demo 1/Form1.Designer.cs
+++ CIS 209 Chapter 5 Demo 1/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblChoice = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbxChoices = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pick Your Ice Cream Flavor";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(134, 224);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 26);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblChoice
+            // 
+            this.lblChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChoice.Location = new System.Drawing.Point(16, 177);
+            this.lblChoice.Name = "lblChoice";
+            this.lblChoice.Size = new System.Drawing.Size(193, 44);
+            this.lblChoice.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lbxChoices);
+            this.panel1.Location = new System.Drawing.Point(13, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 123);
+            this.panel1.TabIndex = 3;
+            // 
+            // lbxChoices
+            // 
+            this.lbxChoices.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxChoices.FormattingEnabled = true;
+            this.lbxChoices.ItemHeight = 18;
+            this.lbxChoices.Location = new System.Drawing.Point(5, 4);
+            this.lbxChoices.Name = "lbxChoices";
+            this.lbxChoices.Size = new System.Drawing.Size(188, 112);
+            this.lbxChoices.TabIndex = 0;
+            this.lbxChoices.SelectedIndexChanged += new System.EventHandler(this.lbxChoices_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(221, 262);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblChoice);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "U-Serve Ice Cream Parlor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblChoice;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox lbxChoices;
     }
 }
 
--- CIS 209 Chapter 5 Demo 1/Form1.cs
+++ CIS 209 Chapter 5 Demo 1/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace CIS_209_Chapter_5_Demo_1
@@ -15,6 +9,45 @@ namespace CIS_209_Chapter_5_Demo_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declare a variable for the flavor
+                string flavor;
+
+                // Open a file for reading
+                StreamReader inputFile = File.OpenText("flavors.txt");
+
+                // Read the file's contents
+                while (!inputFile.EndOfStream)
+                {
+                    // Read the next line
+                    flavor = inputFile.ReadLine();
+
+                    // Add it to the list box
+                    lbxChoices.Items.Add(flavor);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "U-Sever Ice Cream Parlor",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+
+        private void lbxChoices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string choice = lbxChoices.SelectedItem.ToString();
+            lblChoice.Text = "Enjoy your " + choice + " ice cream!";
         }
     }
 }
